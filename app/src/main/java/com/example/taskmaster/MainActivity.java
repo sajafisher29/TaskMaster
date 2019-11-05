119400d237df46a5a8780c266f57d2f2d8dd6501
Sarah Fisher <sajafisher@gmail.com>
2019-11-05 11:28:39 -0800

    Log in and Log out buttons and recycler view running. Needs editing on layout

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -15,10 +15,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.amazonaws.amplify.generated.graphql.ListTasksQuery;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.SignInUIOptions;
+import com.amazonaws.mobile.client.UserStateDetails;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
 import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
 import com.apollographql.apollo.GraphQLCall;
+import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 import java.util.LinkedList;
 import java.util.List;
@@ -40,11 +44,9 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTas
     @Override
     protected void onResume() {
         super.onResume();
-        // Grab username from SharedPreferences and use it to update the user's name displayed
-        SharedPreferences usernameSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String username = usernameSharedPreferences.getString("username", "user");
+        String username = AWSMobileClient.getInstance().getUsername();
         TextView nameTextView = findViewById(R.id.greetingTextView);
-        nameTextView.setText("Hello " + username + "!"); // Strings are coded to replace this. Needs to be refactored.
+        nameTextView.setText("Hello " + username + "!"); // Strings are coded to replace this. Needs to be refactored so it is translatable.
 
         runQuery();
     }
@@ -52,133 +54,155 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnTas
     public void runQuery(){
         awsAppSyncClient.query(ListTasksQuery.builder().build())
                 .responseFetcher(AppSyncResponseFetchers.NETWORK_ONLY)
-                .enqueue(tasksCallback);
+                .enqueue(getAllTasksCallback);
     }
 
-    private GraphQLCall.Callback<ListTasksQuery.Data> tasksCallback = new GraphQLCall.Callback<ListTasksQuery.Data>() {
+    private final GraphQLCall.Callback<ListTasksQuery.Data> getAllTasksCallback = new GraphQLCall.Callback<ListTasksQuery.Data>() {
         @Override
-        public void onResponse(@Nonnull com.apollographql.apollo.api.Response<ListTasksQuery.Data> response) {
+        public void onResponse(@Nonnull Response<ListTasksQuery.Data> response) {
             Log.i("Results", response.data().listTasks().items().toString());
             Handler handlerForMainThread = new Handler(Looper.getMainLooper()) {
                 @Override
                 public void handleMessage(Message inputMessageToMain){
-                    ListTasksQuery.Data tasksQuery = response.data();
-                    Log.i("Results", "handing response");
-                    // Take items and turn them into tasks
+
+                    List<ListTasksQuery.Item> items = response.data().listTasks().items();
                     tasks.clear();
-                    for(ListTasksQuery.Data data : tasks) {
-                        tasks.add(new Task(task));
+                    for (ListTasksQuery.Item item : items) {
+                        tasks.add(new Task(item.name(), item.description()));
                     }
-                    // Feed tasks to taskArray
-
-                    // Notify adapter of updated data
-
+                    recyclerView.getAdapter().notifyDataSetChanged();
                 }
             };
-            Message completeMessage = handlerForMainThread.obtainMessage(0, response);
+                        Message completeMessage = handlerForMainThread.obtainMessage(0, response);
             completeMessage.sendToTarget();
+            };
+
+            @Override
+            public void onFailure (@Nonnull ApolloException error){
+                Log.e("ERROR", error.toString());
+            }
         }
 
+        ;
+
+        // This gets called automatically when MainActivity is created/shown for the first time
         @Override
-        public void onFailure(@Nonnull ApolloException error) {
-            Log.e("ERROR", error.toString());
-        }
-    };
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    // This gets called automatically when MainActivity is created/shown for the first time
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            AWSMobileClient.getInstance().initialize(getApplicationContext(), new com.amazonaws.mobile.client.Callback<UserStateDetails>() {
+                @Override
+                public void onResult(UserStateDetails result) {
+                    Log.i("INIT", "onResult: " + result.getUserState().toString());
+                    if (result.getUserState().toString().equals("SIGN_OUT")) {
+                        AWSMobileClient.getInstance().showSignIn(MainActivity.this,
+                            SignInUIOptions.builder().backgroundColor(1).logo(R.drawable.leaftaskmasterapp).build(),
+                            new com.amazonaws.mobile.client.Callback<UserStateDetails>() {
+                                @Override
+                                public void onResult(UserStateDetails result) {
+                                    Log.i("sign_in", result.getUserState().toString());
+                                }
 
-        tasks = new LinkedList<Task>();
+                                @Override
+                                public void onError(Exception error) {
+                                    Log.e("sign_in", error.getMessage());
+                                }
+                            });
+                    }
+                }
 
-        // Set up RecyclerView
-        recyclerView = findViewById(R.id.mainRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        taskAdapter = new TaskAdapter(tasks, this);
-        recyclerView.setAdapter(taskAdapter);
+                @Override
+                public void onError(Exception error) {
+                    Log.e("INIT", "Initialization error.", error);
+                }
+            });
 
-        // Connect to AWS
-        awsAppSyncClient = AWSAppSyncClient.builder()
-                .context(getApplicationContext())
-                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
-                .build();
+            // Connect to AWS
+            awsAppSyncClient = AWSAppSyncClient.builder()
+                    .context(getApplicationContext())
+                    .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                    .build();
+
+            tasks = new LinkedList<Task>();
+
+            // Set up RecyclerView
+            recyclerView = findViewById(R.id.mainRecyclerView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            taskAdapter = new TaskAdapter(tasks, this);
+            recyclerView.setAdapter(taskAdapter);
+
+
+
+
+
+            Button signInButton = findViewById(R.id.signInButton);
+            signInButton.setOnClickListener((event) -> {
+                // Add the sign in
+                // 'this' refers the the current active activity, probably replace with MainActivity.this
+                AWSMobileClient.getInstance().showSignIn(MainActivity.this, SignInUIOptions.builder().build(), new com.amazonaws.mobile.client.Callback<UserStateDetails>() {
+                    @Override
+                    public void onResult(UserStateDetails result) {
+                        Log.d(TAG, "onResult: " + result.getUserState());
+                    }
+
+                    @Override
+                    public void onError(Exception error) {
+                        Log.e(TAG, "onError: ", error);
+                    }
+                });
+            });
 
         // Grab the add a task button
         Button addTaskButton = findViewById(R.id.addTaskButton);
         // Add an event listener
-        addTaskButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View event) {
-                Intent goToAddATaskActivityIntent = new Intent(MainActivity.this, AddATask.class);
-                MainActivity.this.startActivity(goToAddATaskActivityIntent);
-            }
-        });
+            addTaskButton.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View event) {
+                     Intent goToAddATaskActivityIntent = new Intent(MainActivity.this, AddATask.class);
+                     MainActivity.this.startActivity(goToAddATaskActivityIntent);
+                 }
+            });
 
         // Grab the all tasks button
         Button allTasksButton = findViewById(R.id.allTasksButton);
         // Add an event listener
-        allTasksButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View event) {
+            allTasksButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick (View event){
                 Intent goToAllTasksActivityIntent = new Intent(MainActivity.this, AllTasks.class);
                 MainActivity.this.startActivity(goToAllTasksActivityIntent);
-            }
-        });
+        }
+            });
 
         // Grab the settings button
         Button settingsButton = findViewById(R.id.settingsPageButton);
         // Add an event listener
-        settingsButton.setOnClickListener(new View.OnClickListener() {
+            settingsButton.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
-            public void onClick(View event) {
-                Intent goToSettingsActivityIntent = new Intent(MainActivity.this, Settings.class);
-                MainActivity.this.startActivity(goToSettingsActivityIntent);
-            }
+            public void onClick (View event){
+            Intent goToSettingsActivityIntent = new Intent(MainActivity.this, Settings.class);
+            MainActivity.this.startActivity(goToSettingsActivityIntent);
+        }
         });
 
     }
 
-    public void taskSelected(Task task) {
-        Intent goToTaskDetailsPageActivityIntent = new Intent(this, TaskDetails.class);
+        public void taskSelected(Task task) {
+            Intent goToTaskDetailsPageActivityIntent = new Intent(this, TaskDetails.class);
 
-        // Add info about what task is being checked
-        goToTaskDetailsPageActivityIntent.putExtra("taskTitle", task.getTitle());
-        goToTaskDetailsPageActivityIntent.putExtra("taskBody", task.getBody());
-        Log.i(TAG, "inside taskSelected trying to move to Task Title " + task.getTitle());
-        MainActivity.this.startActivity(goToTaskDetailsPageActivityIntent);
+            // Add info about what task is being checked
+            goToTaskDetailsPageActivityIntent.putExtra("taskTitle", task.getTitle());
+            goToTaskDetailsPageActivityIntent.putExtra("taskBody", task.getBody());
+            Log.i(TAG, "inside taskSelected trying to move to Task Title " + task.getTitle());
+            MainActivity.this.startActivity(goToTaskDetailsPageActivityIntent);
+        }
+
+        public static final String taskTitle = "taskTitle";
+
     }
-
-    public static final String taskTitle = "taskTitle";
-
-//    public void putDataOnPage(String data){
-//
-//        // Turn JSON into InternetTasks
-//        Gson gson = new Gson();
-//        InternetTask[] incomingArray = gson.fromJson(data, InternetTask[].class);
-//
-//        //Getting a set of existing titles
-//        HashSet<String> titles = new HashSet<>();
-//
-//        for(Task task : this.tasks){
-//            titles.add(task.getTitle());
-//        }
-//
-//        for(InternetTask internetTask: incomingArray){
-//            //if the title is a new title then add it
-//            if(!titles.contains(internetTask.getTitle())){
-//                Task newTask = new Task(internetTask.getTitle(), internetTask.getBody());
-////                titles.add(newTask);
-//                database.taskDao().addTask(newTask);
-//            }
-//        }
-//
-//        taskAdapter.notifyDataSetChanged();
-//
-//    }
-
-}
 
 abstract class LogDataWhenItComesBackCallback implements Callback {
 
