ccfbb742bf8029211d2410452b45ec0df3721c04
Sarah Fisher <sajafisher@gmail.com>
2019-11-12 13:45:32 -0800

    Working on building media download and display before working on sharing media to the app

--- app/src/main/java/com/example/taskmaster/TaskDetails.java
+++ app/src/main/java/com/example/taskmaster/TaskDetails.java
@@ -2,9 +2,17 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
+import com.squareup.picasso.Picasso;
+
 public class TaskDetails extends AppCompatActivity {
+
+    private AWSAppSyncClient awsAppSyncClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +24,21 @@ public class TaskDetails extends AppCompatActivity {
         tasktitle.setText(taskName);
 
         String taskDescription = getIntent().getStringExtra("taskBody");
-        TextView taskDesc =  findViewById(R.id.taskDetailsDescription);
+        TextView taskDesc = findViewById(R.id.taskDetailsDescription);
         taskDesc.setText(taskDescription);
-    }
 
+        // Build a connection to AWS
+        awsAppSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .build();
+
+        String filePath = getIntent().getStringExtra("filePath");
+
+        if (filePath != null && filePath.length() > 2) {
+            ImageView taskImage = findViewById(R.id.mediaPlaceholder);
+            Picasso.get().load(filePath).into(taskImage);
+
+        }
+    }
 }
